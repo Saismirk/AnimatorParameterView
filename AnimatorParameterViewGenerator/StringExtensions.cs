1a679488f9f2122287d6bf125c8305f1583009f5
Saismirk <ndgonzal@uwaterloo.ca>
2023-12-28 16:43:30 -0500

    Update StringExtensions.cs

--- AnimatorParameterViewGenerator/StringExtensions.cs
+++ AnimatorParameterViewGenerator/StringExtensions.cs
@@ -1,7 +1,14 @@
+using System.Reflection;
+using System.Text.RegularExpressions;
+
 namespace AnimatorParameterViewGenerator;
 
 public static class StringExtensions {
     public static string[] Split(this string str, string separator) {
-        return str.Split(new[] {separator}, StringSplitOptions.None);
+        return str.Split(new[] { separator }, StringSplitOptions.None);
     }
+
+    public static long ToLong(this string str) => long.TryParse(str, out var result) ? result : 0;
+    public static int ToInt(this string str) => int.TryParse(str, out var result) ? result : 0;
+    public static bool ToBool(this string str) => bool.TryParse(str, out var result) && result;
 }
